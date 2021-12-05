dd354bcde66a9da87287003ed55b2e88462f3970
Kim Chin <>
2021-12-04 22:51:25 -0500

    packege change

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -4,9 +4,14 @@ import Card from './Card';
 import Button from './Button';
 import classes from './ErrorModal.module.css';
 
+const Backdrop = (props) => {
+  return <div className={classes.backdrop}
+  onClick={props.onConfirm} />
+}
+
 const ErrorModal = (props) => {
   return (
-    <div>
+    <React.Fragment>
       <div className={classes.backdrop} onClick={props.onConfirm} />
       <Card className={classes.modal}>
         <header className={classes.header}>
@@ -19,7 +24,7 @@ const ErrorModal = (props) => {
           <Button onClick={props.onConfirm}>Okay</Button>
         </footer>
       </Card>
-    </div>
+    </React.Fragment>
   );
 };
 
